c46bda500cc2c38ede12d21ad0530d5a09f8ac3c
DESKTOP-0R12G3H\ntt <tucothien@gmail.com>
2024-09-08 19:54:26 +0900

    Remove duplicated mixture

--- demo/data/meta_data.js
+++ demo/data/meta_data.js
@@ -18,11 +18,6 @@ dataObject = {
 					"caption": "The crowd is cheering, yelling, whistling, and applauding enthusiastically."
 				},
 				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-17.png",
-					"audio": "data/real_examples/audios/test-real-case-17.wav"
-				},
-				{
 					"method_name": "estimated",
 					"image": "data/real_examples/images/test-real-case-17_1.png",
 					"audio": "data/real_examples/audios/test-real-case-17_1.wav",
@@ -44,20 +39,10 @@ dataObject = {
 					"caption": "Clear footsteps keep coming from afar."
 				},
 				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-26.png",
-					"audio": "data/real_examples/audios/test-real-case-26.wav"
-				},
-				{
 					"method_name": "estimated",
 					"image": "data/real_examples/images/test-real-case-26_1.png",
 					"audio": "data/real_examples/audios/test-real-case-26_1.wav",
 					"caption": "A lot of insects are chirping."
-				},
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-26.png",
-					"audio": "data/real_examples/audios/test-real-case-26.wav"
 				},
 				{
 					"method_name": "estimated",
@@ -79,11 +64,6 @@ dataObject = {
 					"image": "data/real_examples/images/test-real-case-86_0.png",
 					"audio": "data/real_examples/audios/test-real-case-86_0.wav",
 					"caption": "the wind is blowing hard on the roadside."
-				},
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-86.png",
-					"audio": "data/real_examples/audios/test-real-case-86.wav"
 				},
 				{
 					"method_name": "estimated",
@@ -120,11 +100,6 @@ dataObject = {
 					"image": "data/real_examples/images/test-real-case-185_0.png",
 					"audio": "data/real_examples/audios/test-real-case-185_0.wav",
 					"caption": "the train is moving along the tracks, producing a low rumble."
-				},
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-185.png",
-					"audio": "data/real_examples/audios/test-real-case-185.wav"
 				},
 				{
 					"method_name": "estimated",
