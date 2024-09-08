2fc11165ec04b6182792183ad37584ed695b05ed
Cao Hai Nhi <nhicao1995@gmail.com>
2024-09-08 20:27:52 +0700

    add title and text to meta_data

--- demo/data/meta_data.js
+++ demo/data/meta_data.js
@@ -1,137 +1,272 @@
-dataObject = {
-    "method_names": [
-        "Mixture",
-        "Extracted sources",
-    ],
-    "data": [
-		{
-			"methods": [
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-17.png",
-					"audio": "data/real_examples/audios/test-real-case-17.wav"
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-17_0.png",
-					"audio": "data/real_examples/audios/test-real-case-17_0.wav",
-					"caption": "The crowd is cheering, yelling, whistling, and applauding enthusiastically."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-17_1.png",
-					"audio": "data/real_examples/audios/test-real-case-17_1.wav",
-					"caption": "A camera is shooting."
-				}
-			]
-		},
-		{
-			"methods": [
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-26.png",
-					"audio": "data/real_examples/audios/test-real-case-26.wav"
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-26_0.png",
-					"audio": "data/real_examples/audios/test-real-case-26_0.wav",
-					"caption": "Clear footsteps keep coming from afar."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-26_1.png",
-					"audio": "data/real_examples/audios/test-real-case-26_1.wav",
-					"caption": "A lot of insects are chirping."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-26_2.png",
-					"audio": "data/real_examples/audios/test-real-case-26_2.wav",
-					"caption": "A dog is barking."
-				}
-			]
-		},
-		{
-			"methods": [
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-86.png",
-					"audio": "data/real_examples/audios/test-real-case-86.wav"
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-86_0.png",
-					"audio": "data/real_examples/audios/test-real-case-86_0.wav",
-					"caption": "The wind is blowing hard on the roadside."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-86_1.png",
-					"audio": "data/real_examples/audios/test-real-case-86_1.wav",
-					"caption": "Water is droping."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-86_2.png",
-					"audio": "data/real_examples/audios/test-real-case-86_2.wav",
-					"caption": "The dripping water trickles."
-				}
-			]
-		},
-		{
-			"methods": [
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-170.png",
-					"audio": "data/real_examples/audios/test-real-case-170.wav"
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-170_0.png",
-					"audio": "data/real_examples/audios/test-real-case-170_0.wav",
-					"caption": "The bell is ringing with faint echoes."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-170_1.png",
-					"audio": "data/real_examples/audios/test-real-case-170_1.wav",
-					"caption": "A bell is rung a couple of times."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-170_2.png",
-					"audio": "data/real_examples/audios/test-real-case-170_2.wav",
-					"caption": "A loud and pleasant bell chimes several times."
-				}
-			]
-		},
-		{
-			"methods": [
-				{
-					"method_name": "mixture",
-					"image": "data/real_examples/images/test-real-case-185.png",
-					"audio": "data/real_examples/audios/test-real-case-185.wav"
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-185_0.png",
-					"audio": "data/real_examples/audios/test-real-case-185_0.wav",
-					"caption": "The train is moving along the tracks, producing a low rumble."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-185_1.png",
-					"audio": "data/real_examples/audios/test-real-case-185_1.wav",
-					"caption": "A train is rolling down the train tracks."
-				},
-				{
-					"method_name": "estimated",
-					"image": "data/real_examples/images/test-real-case-185_2.png",
-					"audio": "data/real_examples/audios/test-real-case-185_2.wav",
-					"caption": "Horns are honking."
-				}
-			]
-		}
-	]
-};
+dataObject = [
+	{
+		"title": "Title 1 - Setting one column width",
+		"text": "Use our powerful mobile-first flexbox grid to build layouts of all shapes and sizes thanks to a twelve column system, five default responsive tiers, Sass variables and mixins, and dozens of predefined classes.",
+		"data": [
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-17.png",
+						"audio": "data/real_examples/audios/test-real-case-17.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-17_0.png",
+						"audio": "data/real_examples/audios/test-real-case-17_0.wav",
+						"caption": "The crowd is cheering, yelling, whistling, and applauding enthusiastically."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-17_1.png",
+						"audio": "data/real_examples/audios/test-real-case-17_1.wav",
+						"caption": "A camera is shooting."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-26.png",
+						"audio": "data/real_examples/audios/test-real-case-26.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-26_0.png",
+						"audio": "data/real_examples/audios/test-real-case-26_0.wav",
+						"caption": "Clear footsteps keep coming from afar."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-26_1.png",
+						"audio": "data/real_examples/audios/test-real-case-26_1.wav",
+						"caption": "A lot of insects are chirping."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-26_2.png",
+						"audio": "data/real_examples/audios/test-real-case-26_2.wav",
+						"caption": "A dog is barking."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-86.png",
+						"audio": "data/real_examples/audios/test-real-case-86.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-86_0.png",
+						"audio": "data/real_examples/audios/test-real-case-86_0.wav",
+						"caption": "The wind is blowing hard on the roadside."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-86_1.png",
+						"audio": "data/real_examples/audios/test-real-case-86_1.wav",
+						"caption": "Water is droping."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-86_2.png",
+						"audio": "data/real_examples/audios/test-real-case-86_2.wav",
+						"caption": "The dripping water trickles."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-170.png",
+						"audio": "data/real_examples/audios/test-real-case-170.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-170_0.png",
+						"audio": "data/real_examples/audios/test-real-case-170_0.wav",
+						"caption": "The bell is ringing with faint echoes."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-170_1.png",
+						"audio": "data/real_examples/audios/test-real-case-170_1.wav",
+						"caption": "A bell is rung a couple of times."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-170_2.png",
+						"audio": "data/real_examples/audios/test-real-case-170_2.wav",
+						"caption": "A loud and pleasant bell chimes several times."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-185.png",
+						"audio": "data/real_examples/audios/test-real-case-185.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-185_0.png",
+						"audio": "data/real_examples/audios/test-real-case-185_0.wav",
+						"caption": "The train is moving along the tracks, producing a low rumble."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-185_1.png",
+						"audio": "data/real_examples/audios/test-real-case-185_1.wav",
+						"caption": "A train is rolling down the train tracks."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-185_2.png",
+						"audio": "data/real_examples/audios/test-real-case-185_2.wav",
+						"caption": "Horns are honking."
+					}
+				]
+			}
+		]
+	},
+	{
+		"title": "Title 2 - Setting one column width",
+		"text": "The viewport is the user's visible area of a web page.\nThe viewport varies with the device, and will be smaller on a mobile phone than on a computer screen.\nBefore tablets and mobile phones, web pages were designed only for computer screens, and it was common for web pages to have a static design and a fixed size.",
+		"data": [
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-17.png",
+						"audio": "data/real_examples/audios/test-real-case-17.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-17_0.png",
+						"audio": "data/real_examples/audios/test-real-case-17_0.wav",
+						"caption": "The crowd is cheering, yelling, whistling, and applauding enthusiastically."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-17_1.png",
+						"audio": "data/real_examples/audios/test-real-case-17_1.wav",
+						"caption": "A camera is shooting."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-26.png",
+						"audio": "data/real_examples/audios/test-real-case-26.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-26_0.png",
+						"audio": "data/real_examples/audios/test-real-case-26_0.wav",
+						"caption": "Clear footsteps keep coming from afar."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-26_1.png",
+						"audio": "data/real_examples/audios/test-real-case-26_1.wav",
+						"caption": "A lot of insects are chirping."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-26_2.png",
+						"audio": "data/real_examples/audios/test-real-case-26_2.wav",
+						"caption": "A dog is barking."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-86.png",
+						"audio": "data/real_examples/audios/test-real-case-86.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-86_0.png",
+						"audio": "data/real_examples/audios/test-real-case-86_0.wav",
+						"caption": "The wind is blowing hard on the roadside."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-86_1.png",
+						"audio": "data/real_examples/audios/test-real-case-86_1.wav",
+						"caption": "Water is droping."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-86_2.png",
+						"audio": "data/real_examples/audios/test-real-case-86_2.wav",
+						"caption": "The dripping water trickles."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-170.png",
+						"audio": "data/real_examples/audios/test-real-case-170.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-170_0.png",
+						"audio": "data/real_examples/audios/test-real-case-170_0.wav",
+						"caption": "The bell is ringing with faint echoes."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-170_1.png",
+						"audio": "data/real_examples/audios/test-real-case-170_1.wav",
+						"caption": "A bell is rung a couple of times."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-170_2.png",
+						"audio": "data/real_examples/audios/test-real-case-170_2.wav",
+						"caption": "A loud and pleasant bell chimes several times."
+					}
+				]
+			},
+			{
+				"methods": [
+					{
+						"method_name": "mixture",
+						"image": "data/real_examples/images/test-real-case-185.png",
+						"audio": "data/real_examples/audios/test-real-case-185.wav"
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-185_0.png",
+						"audio": "data/real_examples/audios/test-real-case-185_0.wav",
+						"caption": "The train is moving along the tracks, producing a low rumble."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-185_1.png",
+						"audio": "data/real_examples/audios/test-real-case-185_1.wav",
+						"caption": "A train is rolling down the train tracks."
+					},
+					{
+						"method_name": "estimated",
+						"image": "data/real_examples/images/test-real-case-185_2.png",
+						"audio": "data/real_examples/audios/test-real-case-185_2.wav",
+						"caption": "Horns are honking."
+					}
+				]
+			}
+		]
+	}
+];
